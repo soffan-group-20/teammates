86a676fc7088e8573cf4bfa1c713942857993ded
agenell <agenell@kth.se>
2023-02-21 11:09:00 +0100

    fix: typo in visibility-entity-name.pipe.spec.ts

--- src/web/app/components/visibility-messages/visibility-entity-name.pipe.spec.ts
+++ src/web/app/components/visibility-messages/visibility-entity-name.pipe.spec.ts
@@ -9,7 +9,7 @@ describe('VisibilityEntityNamePipe', () => {
     expect(pipe).toBeTruthy();
   });
 
-  it('should return instructor when given the appropriate enum', () => {
+  it('should return team members when given the appropriate enum', () => {
     const pipe: VisibilityEntityNamePipe = new VisibilityEntityNamePipe();
     const str = pipe.transform(FeedbackVisibilityType.RECIPIENT_TEAM_MEMBERS,);
     expect(str).toEqual("The recipient's team members");
