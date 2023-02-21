daa50d906f9bfce758cc777eeed443ade370c10d
agenell <agenell@kth.se>
2023-02-21 07:12:12 +0100

    fix: add test case for transform (#52)

--- src/web/app/components/visibility-messages/visibility-capability.pipe.spec.ts
+++ src/web/app/components/visibility-messages/visibility-capability.pipe.spec.ts
@@ -5,4 +5,12 @@ describe('VisibilityCapabilityPipe', () => {
     const pipe: VisibilityCapabilityPipe = new VisibilityCapabilityPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should return the correct string for the given arguments', () => {
+    const pipe: VisibilityCapabilityPipe = new VisibilityCapabilityPipe();
+    const str = pipe.transform({SHOW_RESPONSE: true,
+                                SHOW_GIVER_NAME: true,
+                                SHOW_RECIPIENT_NAME: false});
+    expect(str).toEqual('can see your response, and your name, but not the name of the recipient');
+  })
 });
