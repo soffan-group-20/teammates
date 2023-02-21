1699521c88c0efc5b6ad412add76b472af324309
agenell <agenell@kth.se>
2023-02-21 10:52:55 +0100

    fix: add test case for transform (#56)

--- src/web/app/components/visibility-messages/visibility-entity-name.pipe.spec.ts
+++ src/web/app/components/visibility-messages/visibility-entity-name.pipe.spec.ts
@@ -1,8 +1,19 @@
 import { VisibilityEntityNamePipe } from './visibility-entity-name.pipe';
+import {
+  FeedbackParticipantType,
+  FeedbackVisibilityType,
+} from '../../../types/api-output';
 
 describe('VisibilityEntityNamePipe', () => {
   it('create an instance', () => {
     const pipe: VisibilityEntityNamePipe = new VisibilityEntityNamePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should return instructor when given the appropriate enum', () => {
+    const pipe: VisibilityEntityNamePipe = new VisibilityEntityNamePipe();
+    const str = pipe.transform(FeedbackVisibilityType.RECIPIENT,
+                               FeedbackParticipantType.INSTRUCTORS);
+    expect(str).toEqual('The receiving instructor');
   });
 });
